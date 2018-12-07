d4c1e308b8fa26bfef92d2a8d07d075754dd56a3
javadongge <easthome66@163.com>
2018-12-07 08:45:02 +0800

    组员加入了一个后端控制器请求

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
@@ -12,5 +13,11 @@ public class IndexController {
     public String one(Map<String,Object> model){
         model.put("score",68);
         return "one";
+    }
+    
+    @RequestMapping("/two.html")
+    public String two(Model model) {
+    	model.addAttribute("score", 18);
+    	return "one";
     }
 }
